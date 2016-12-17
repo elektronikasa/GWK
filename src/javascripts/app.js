62f5c8fc303a04d6169e383f8a6e1330484c1a60
elektronikasa <jimmy@Jimmys-MacBook-Air.local>
2016-12-17 13:57:51 +0200

    Latest commit

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,7 +1,8 @@
 var modernizr = require('./third-party/modernizr-custom');
-
 /* Your app code here. */
 //Velocity(document.body, { opacity: 0.5 });
 import './modules'
+var FastClick = require('./third-party/fastclick');
+FastClick.attach(document.body);
 
 console.log('app.js has loaded!');
